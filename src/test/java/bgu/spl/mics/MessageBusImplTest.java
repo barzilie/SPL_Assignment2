7674dfd35875adfeedf9954d0895b330bfb27406
friedami <friedami@DESKTOP-SSR6GDG.>
2024-12-23 20:22:18 +0200

    messageBus fixes

--- src/test/java/bgu/spl/mics/MessageBusImplTest.java
+++ src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -1,59 +1,77 @@
 package bgu.spl.mics;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-/**
- * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
- * Write your implementation here!
- * Only private fields and methods can be added to this class.
- */
-public class MessageBusImpl implements MessageBus {
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.*;
 
-	@Override
-	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+
+public class MessageBusImplTest {
+	private static MessageBusImpl instance; 
+
+	//Set up for a test.
+     
+    @BeforeEach 
+	public void setUp() throws Exception {
+        MessageBusImpl mb = instance.getInstance();
+    }
+
+	@Test
+	public void testGetInstance() {
+
+	}
+
+	@Test
+	public void testSubscribeEvent() {
+	//	this.instance.subscribeEvent(new Event() {;},new MicroService("Test1") )
+	//	getEventSubscribers
+	}
+
+	@Test
+	public void testSubscribeBroadcast() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+	@Test
+	public void testComplete() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void sendBroadcast(Broadcast b) {
+	@Test
+	public void testSendBroadcast() {
 		// TODO Auto-generated method stub
 
 	}
 
 	
-	@Override
-	public <T> Future<T> sendEvent(Event<T> e) {
+	@Test
+	public void testSendEvent() {
 		// TODO Auto-generated method stub
-		return null;
-	}
+=	}
 
-	@Override
-	public void register(MicroService m) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void unregister(MicroService m) {
+	@Test
+	public void testRegister() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public Message awaitMessage(MicroService m) throws InterruptedException {
+	@Test
+	public void testUnregister() {
 		// TODO Auto-generated method stub
-		return null;
+
+	}
+
+	@Test
+	public void testAwaitMessage() throws InterruptedException {
+		// TODO Auto-generated method stub
+=	}
+
+	@Test
+	public void testRoundRobin(){
+
 	}
 
 	
